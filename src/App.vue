851d3b619b556e685fd2c496e395815259e313a6
Vitaly Petrovich <spacebndx@gmail.com>
2022-02-08 13:50:36 +0700

    init

---
+++ src/App.vue
@@ -0,0 +1,10 @@
+<template>
+  <Dashboard />
+</template>
+
+<script lang="ts">
+import Dashboard from '@/views/Dashboard.vue'
+export default {
+  components: { Dashboard },
+}
+</script>
---
+++ src/views/Dashboard.vue
@@ -0,0 +1,9 @@
+<template>Dashboard</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  name: 'Dashboard',
+})
+</script>
