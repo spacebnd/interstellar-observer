f2d9a0e781509c440364a2aec4b2209af114cb43
Vitaly Petrovich <spacebndx@gmail.com>
2022-02-11 14:16:10 +0700

    refactor(common): add constants

---
+++ src/common/constants.ts
@@ -0,0 +1,46 @@
+const APP_MODULES = {
+  DASHBOARD: 'DASHBOARD',
+  EARTH: 'EARTH',
+  MARS: 'MARS',
+  ISS: 'ISS', // International Space Station
+  PEOPLE_IN_SPACE: 'PEOPLE_IN_SPACE',
+  SOLAR_SYSTEM: 'SOLAR_SYSTEM',
+  APOD: 'APOD', // Astronomy Picture of the Day
+  NASA_LIBRARY: 'NASA_LIBRARY',
+  SPACE_WEATHER: 'SPACE_WEATHER',
+  ALL_NEWS: 'ALL_NEWS',
+}
+
+const API_MODULES = {
+  NASA_APOD: 'NASA_APOD', // NASA Astronomy Picture of the Day
+  NASA_DONKI: 'NASA_DONKI', // NASA Space Weather Database Of Notifications, Knowledge, Information
+  NASA_EPIC: 'NASA_EPIC', // NASA Earth Polychromatic Imaging Camera
+  NASA_MARS_ROVER: 'NASA_MARS_ROVER', // NASA Mars Rover Photos
+  NASA_LIBRARY: 'NASA_LIBRARY', // NASA Image and Video Library
+  NASA_HORIZONS: 'NASA_HORIZONS', // NASA Horizons System
+  SOLAR_SYSTEM_DATA: 'SOLAR_SYSTEM_DATA', // The Solar System OpenData
+  ISS_LIVE: 'ISS_LIVE', // ISS High Definition Live Streaming Video of the Earth
+  WTIA: 'WTIA', // Where the International Space Station at
+  PISRN: 'PISRN', // People in Space Right Now
+  SPACEFLIGHT_NEWS: 'SPACEFLIGHT_NEWS', // Spaceflight News
+  TWITTER: 'TWITTER', // Embedded Twitter Timelines
+}
+
+const TWITTER_ACCOUNTS = {
+  ISS: 'Space_Station',
+  ISS_RESEARCH: 'ISS_Research',
+  NASA: 'nasa',
+  NASA_EARTH: 'Nasaearth',
+  NASA_MOON: 'NASAMoon',
+  NASA_MARS: 'Nasamars',
+  NASA_MARS_CURIOSITY: 'marscuriosity',
+  NASA_SUN: 'NASASun',
+  NASA_SOLAR_SYSTEM: 'NASASolarSystem',
+  NASA_JPL: 'nasajpl',
+  NASA_HUBBLE: 'nasahubble',
+  NASA_WEBB: 'NASAWebb',
+  NASA_UNIVERSE: 'NASAUniverse',
+  NASA_EXOPLANETS: 'NASAExoplanets',
+}
+
+export { APP_MODULES, API_MODULES, TWITTER_ACCOUNTS }
