33a9185d6bb3917ec38d40af2b7cc20f1bfcb1eb
Vitaly Petrovich <spacebndx@gmail.com>
2022-03-10 14:38:42 +0700

    deploy

---
+++ precache-manifest.d68d4746a220e0bdc95a224432a9a671.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "970a74acd126472c8ff78478a2e41655",
+    "url": "/interstellar-observer/index.html"
+  },
+  {
+    "revision": "e62779e5ee15efaec168",
+    "url": "/interstellar-observer/js/app.4831698d.js"
+  },
+  {
+    "revision": "05af1b9ead051d19f266",
+    "url": "/interstellar-observer/js/chunk-vendors.83037900.js"
+  },
+  {
+    "revision": "326011ef93b5ab46ced54d6947c2803b",
+    "url": "/interstellar-observer/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/interstellar-observer/robots.txt"
+  }
+]);
